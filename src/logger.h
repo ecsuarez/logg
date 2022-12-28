b5c9cb842584fddbdc71a65b10f5cc0ee26e38ed
ecsuarez <ernestcsuarez@gmail.com>
2022-12-28 08:23:44 -0500

    Create logger files

---
+++ src/logger.h
@@ -0,0 +1,31 @@
+/*
+ ** This file is part of the logg project.
+ ** Copyright 2021-2022 Ernest C. Suarez <ernestcsuarez@gmail.com>.
+ **
+ ** This program is free software: you can redistribute it and/or modify
+ ** it under the terms of the GNU Lesser General Public License as
+ ** published by the Free Software Foundation, either version 3 of the
+ ** License, or (at your option) any later version.
+ **
+ ** This program is distributed in the hope that it will be useful,
+ ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ ** GNU Lesser General Public License for more details.
+ **
+ ** You should have received a copy of the GNU Lesser General Public License
+ ** along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ **/
+
+
+/// @file logger.h Control Log functions
+
+#ifndef LOGG_LOGGER_H
+#define LOGG_LOGGER_H
+
+#include "logdef.h"
+#include <stdio.h>
+
+
+
+
+#endif // LOGG_LOGGER_H
